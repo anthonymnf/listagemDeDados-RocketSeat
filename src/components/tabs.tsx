fd6274871b309a742604f729a76c7cea3fa42cb0
anthonymnf <anthonymnf30@gmail.com>
2024-02-28 11:39:31 -0300

    tabs remove justify-between

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -3,7 +3,7 @@ import { ListVideo, Tags, Settings, Code2 } from "lucide-react";
 export function Tabs() {
   return (
     <div className="border-b border-zinc-800 py-4">
-      <nav className="flex items-center gap-2 max-w-[1200px] mx-auto flex-wrap justify-between">
+      <nav className="flex items-center gap-2 max-w-[1200px] mx-auto flex-wrap">
         <a
           href=""
           className="py-1.5 px-3 bg-zinc-800 text-zinc-100 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent"
